8fa81026a560c3bd7dc244950c5ceb0a9a2aabad
unknown <aleeesequiel94@gmail.com>
2021-07-09 01:56:45 -0400

    cors

--- server.js
+++ server.js
@@ -6,6 +6,7 @@
 var PORT = process.env.PORT || 5000;
 var express = require('express');
 var app = express();
+var cors = require('cors');
 
 var http = require('http');
 var server = http.Server(app);
@@ -15,6 +16,7 @@ var server = http.Server(app);
 var mysql = require('mysql');
 var bodyParser = require('body-parser');
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
